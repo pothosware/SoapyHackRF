f93f742fe09cac9394f86946ed7ef95ee52027a4
Josh Blum <josh@joshknows.com>
2016-02-21 11:35:10 -0800

    wait for tx to be consumed before switching

--- HackRF_Streaming.cpp
+++ HackRF_Streaming.cpp
@@ -691,6 +691,15 @@ int SoapyHackRF::acquireReadBuffer(
 
 	if ( _current_mode!=TRANSCEIVER_MODE_RX ) {
 
+		//wait for tx to be consumed before switching
+		const auto exitTime = std::chrono::high_resolution_clock::now() + std::chrono::microseconds(timeoutUs);
+		while (true)
+		{
+			std::unique_lock <std::mutex> lock( _buf_mutex );
+			if (_tx_stream->buf_count == 0) break;
+			if (std::chrono::high_resolution_clock::now() > exitTime) return SOAPY_SDR_TIMEOUT;
+		}
+
 		int ret=this->activateStream(stream);
 		if(ret<0) return ret;
 	}
--- self_test.py
+++ self_test.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     hackrf.setSampleRate(SOAPY_SDR_RX, 0, 8e6)
     hackrf.setSampleRate(SOAPY_SDR_TX, 0, 8e6)
 
+    """
     for i in range(5):
         print("  Make rx stream #%d"%i)
         rxStream = hackrf.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [0])
         for j in range(5):
             numSampsTotal = 10000
             print("    Activate, get %d samples, Deactivate #%d"%(numSampsTotal, j))
-            hackrf.activateStream(rxStream, SOAPY_SDR_END_BURST, 0, numSampsTotal)
+            hackrf.activateStream(rxStream)
             buff = np.array([0]*1024, np.complex64)
             while numSampsTotal > 0:
                 sr = hackrf.readStream(rxStream, [buff], buff.size, timeoutUs=int(1e6))
@@ -43,3 +44,38 @@ if __name__ == "__main__":
                 numSampsTotal -= sr.ret
             hackrf.deactivateStream(txStream)
         hackrf.closeStream(txStream)
+    """
+
+    ####################################################################
+    #setup both streams at once
+    ####################################################################
+    rxStream = hackrf.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [0])
+    txStream = hackrf.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32, [0])
+
+    hackrf.activateStream(txStream)
+    hackrf.activateStream(txStream)
+
+    numSampsTotal = 10000
+    hackrf.activateStream(rxStream)
+    buff = np.array([0]*1024, np.complex64)
+    while numSampsTotal > 0:
+        sr = hackrf.readStream(rxStream, [buff], buff.size, timeoutUs=int(1e6))
+        #print sr
+        assert(sr.ret > 0)
+        numSampsTotal -= sr.ret
+
+    numSampsTotal = 10000
+    buff = np.array([0]*1024, np.complex64)
+    while numSampsTotal != 0:
+        size = min(buff.size, numSampsTotal)
+        sr = hackrf.writeStream(txStream, [buff], size)
+        #print sr
+        if not (sr.ret > 0): print("Fail %s, %d"%(sr, numSampsTotal))
+        assert(sr.ret > 0)
+        numSampsTotal -= sr.ret
+
+    hackrf.deactivateStream(txStream)
+    hackrf.deactivateStream(txStream)
+
+    hackrf.closeStream(rxStream)
+    hackrf.closeStream(txStream)
