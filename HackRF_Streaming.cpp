fc5abe2d539bf03d7a71f55a8ad3e86d155485c0
Kevin Mehall <km@kevinmehall.net>
2017-03-09 20:10:20 -0800

    Add myself to copyright

--- HackRF_Streaming.cpp
+++ HackRF_Streaming.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2015-2016 Wei Jiang
  * Copyright (c) 2015-2017 Josh Blum
+ * Copyright (c) 2017 Kevin Mehall
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
